f5a016c2654878063b474f54a8191be43d99f703
Christopher Harvey <christopher.harvey3@gmail.com>
2017-09-03 14:17:46 +0100

    xxx

--- spectrum_empty.f90
+++
@@ -1,7 +0,0 @@
-subroutine spectrum(spectrumfileID,t,x,y,z,gama,ux,uy,uz,t_old,gama_old,ux_old,uy_old,uz_old)
-!---------------------------------------------
-
-! Empth spectrum module, in order to allow compilation without any dependence on external libraries.
-
-
-end subroutine spectrum
